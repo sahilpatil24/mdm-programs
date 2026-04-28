ead4ff05a94ffdfb6084f4a66d06c411ded17634
Sahil <105579851+sahilpatil24@users.noreply.github.com>
2026-04-28 16:26:36 +0530

    Refactor variables and enhance time resolution
    Updated variable names to lowercase and increased time resolution for better frequency visibility.

--- amfm.py
+++ amfm.py
@@ -1,35 +1,35 @@
-import matplotlib.pyplot as plt
-import numpy as np
+#amfm
+#for fm -> modulation index > 5 for the frequency change to be visible
+#t should be having high resolution like 5000 not 300 is quite low
 
-#define the variables
-fs = 1000
+import numpy as np
+import matplotlib.pyplot as plt
+
+pi = np.pi
 fm = 5
 fc = 100
-Am = 2
-Ac = 5
-t = np.linspace(0,1,300)
-modulation_index = 0.8
+am = 2
+ac = 5
+t = np.linspace(0,1,5000)
+modulation_index = 5
 
-#message and carrier
-message = np.sin(2 * np.pi * fm * t) 
-carrier = Ac * np.cos(2 * np.pi * fc * t)
+message = np.sin(2 * pi * fm * t)
+carrier = ac * np.cos(2 * pi * fc * t)
 
-am_signal = Ac * (1 + modulation_index * message) * np.cos(2 * np.pi * fc * t)
-fm_signal = Ac * np.cos(2 * np.pi * fc * t + modulation_index * np.sin(2 * np.pi * fm * t))
+am_signal = ac * (1 + modulation_index * message) * np.cos(2 * pi * fc * t)
+fm_signal = ac * np.cos(2 * pi * fc * t + modulation_index * np.sin(2 * pi * fm * t))
+
 
 plt.subplot(4,1,1)
-plt.title("Message")
 plt.plot(t,message)
 
 plt.subplot(4,1,2)
-plt.title("Carrier")
 plt.plot(t,carrier)
+
 plt.subplot(4,1,3)
-plt.title("Am")
 plt.plot(t,am_signal)
+
 plt.subplot(4,1,4)
-plt.title("Fm")
 plt.plot(t,fm_signal)
 
-plt.tight_layout()
 plt.show()
